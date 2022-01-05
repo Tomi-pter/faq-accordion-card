edfe0fff8fd903b4d14453f6091c283956e9c425
Tomi Aiyeniko <tomiaiyeniko@gmail.com>
2022-01-05 01:24:54 +0100

    added js for dropdown functionality

---
+++ script.js
@@ -0,0 +1,14 @@
+let btn = document.querySelectorAll(".button");
+
+let btnArr = Array.from(btn);
+console.log(btnArr);
+btnArr.forEach((item) =>
+    item.addEventListener("click", function() {
+        console.log(item.className);
+        if (item.classList.contains("clicked")) {
+            item.classList.remove("clicked");
+        } else {
+            item.classList.add("clicked");
+        }
+    })
+);
